fc9036f1a8563f59eb8eeb0f9367b9f252bc9890
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-06-10 05:03:53 +0200

    Update exlcude list and delte files that are excluded in backup dest dir

--- backup_home_directory_rsync.sh
+++ backup_home_directory_rsync.sh
@@ -29,21 +29,21 @@ case "$1" in
 esac
 
 # Check if there is a backup directory
-if [ ! -z "$1" ]; then
+if [ -n "$1" ]; then
   BACKUP_DIR=$1
 else
   BACKUP_DIR=$DEFAULT_BACKUP_DIR
 fi
 
 # Check if there is a custom home directory
-if [ ! -z "$2" ]; then
+if [ -n "$2" ]; then
   HOME_DIR=$2
 else
   HOME_DIR=$DEFAULT_HOME_DIR
 fi
 
 # Check if the backup directory is a directory
-mkdir -p $BACKUP_DIR
+mkdir -p "$BACKUP_DIR"
 if [ ! -d "$BACKUP_DIR" ]; then
   echo "Error: The backup directory '$BACKUP_DIR' does not exist!"
   exit 1
@@ -59,20 +59,17 @@ echo "Backup '$HOME_DIR' to '$BACKUP_DIR'..."
 
 set -x
 
-# Option: "-v"/"--verbose"   to get a visual progress bar
-# Option: "-r"/"--recursive" to recurse into directories
-# Option: "-a"/"--archive"   to preserve the date, ownership, permissions, groups
-# Option: "--delete"         to delete deleted files in destination directory
+# Option: "-v"/"--verbose"    to get a visual progress bar
+# Option: "-r"/"--recursive"  to recurse into directories
+# Option: "-a"/"--archive"    to preserve the date, ownership, permissions, groups
+# Option: "--delete"          to delete deleted files in destination directory
+# Option: "--delete-excluded" to also delete excluded files in destination directory
 # Option: "--filter=':- .gitignore'" to use the gitignore files when doing a backup
 # Option: "--exclude-from 'rsync_exclude_list.txt'" to ignore certain files from getting a backup
 rsync -r -a -v                                \
       --delete                                \
-      --prune
-      --filter=':- .gitignore'                \
-      --include-from='rsync_include_list.txt' \
       --exclude-from='rsync_exclude_list.txt' \
+      --delete-excluded                       \
       "$HOME_DIR" "$BACKUP_DIR"
-
-# '--include-from' does currently not work in connection with '--exclude-from'
 
 { set +x; } 2>/dev/null
